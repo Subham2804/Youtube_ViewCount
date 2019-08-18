f580189610d6be0305ce4a3110bd556b454917ba
Subham <39151404+Subham2804@users.noreply.github.com>
2019-08-19 00:24:26 +0530

    Add files via upload

---
+++ Youtube_ViewCount.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import requests
+import datetime
+import numpy as np
+from bs4 import BeautifulSoup
+
+f=open('youtube_data.csv','w')
+f.close()
+f=open('youtube_data.csv','a')
+
+
+
+while(True):
+    page=requests.get("https://www.youtube.com/watch?v=TcMBFSGVi1c&t=6s")
+    soup=BeautifulSoup(page.content,'html.parser')
+
+    now=datetime.datetime.now()
+
+    
+    views=soup.find_all(class_='watch-view-count')[0].get_text()
+    views2=views[0:int(len(views))-6]
+    views2=views2.replace(',','')
+    views=int(views2)
+    
+    likes=soup.find_all(class_='yt-uix-button-content')[15].get_text()
+    likes=likes.replace(',','')
+    
+
+
+    dislikes=soup.find_all(class_='yt-uix-button-content')[18].get_text()
+    dislikes=dislikes.replace(',','')
+    
+
+
+    subscribers=soup.find_all(class_='yt-uix-button-content')[20].get_text()
+    subscribers=subscribers.replace(',','')
+
+
+    a=np.asarray([[now.month,now.day,now.hour,now.minute,views,likes,dislikes,subscribers]])
+    np.savetxt(f,a,fmt='%i',delimiter=",")
+  
